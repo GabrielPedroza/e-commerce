7908729ff98819fe51031b5c67ae757d3c02f57d
Gabriel Pedroza <thegabrielpedroza@gmail.com>
2022-04-23 12:26:15 -0400

    🧨Initial Commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
+
+const path = require('path')
+
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+const withSass = require('@zeit/next-sass');
+module.exports = withSass({
+  cssModules: true
+})
+
+module.exports = {
+  /* Add Your Scss File Folder Path Here */
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  nextConfig
+}
+
+
+
+
+
+
+// module.exports = nextConfig
