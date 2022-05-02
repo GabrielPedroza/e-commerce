91e23c552081687930d50f1820760187314ab8ba
Gabriel Pedroza <thegabrielpedroza@gmail.com>
2022-05-02 16:56:52 -0400

    🌲Tree Shook boilerplate

--- next.config.js
+++ next.config.js
@@ -1,27 +1,18 @@
 /** @type {import('next').NextConfig} */
 
 const path = require('path')
-
+const withSass = require('@zeit/next-sass');
 const nextConfig = {
   reactStrictMode: true,
 }
 
-const withSass = require('@zeit/next-sass');
 module.exports = withSass({
   cssModules: true
 })
 
 module.exports = {
-  /* Add Your Scss File Folder Path Here */
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
   nextConfig
 }
-
-
-
-
-
-
-// module.exports = nextConfig
